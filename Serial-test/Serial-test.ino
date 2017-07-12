dc59314686bc7a98f870811da10141835b114928
sumandra majee <skdmajee-git@outlook.com>
2017-07-12 10:22:05 -0700

    Serial test

---
+++ Serial-test/Serial-test.ino
@@ -0,0 +1,16 @@
+#include <SoftwareSerial.h>
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  while (!Serial) {
+    ; //wait for port to be ready
+  }
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println("Hello PI");
+  delay(2000);
+}
